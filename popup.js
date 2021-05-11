c39e9181b6362a2b79dc53c4c93d1e43544f9c7e
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-05-11 21:00:20 +1000

    able to remove both instances of home button

--- popup.js
+++ popup.js
@@ -10,9 +10,24 @@ home_slider.addEventListener("click", async () => {
 });
 
 function removeItems() {
-    let items = document.getElementById("items")
-    if (items != null) {
+    let items = document.getElementById("items").querySelector('[aria-label="Home"]')
+    if (items) {
         items.remove()
-        console.log("hello"); 
+    }
+    // Remove the home icon when the guide button is toggled
+    // Determine if the guide button has been toggled
+    let guideButton = document.getElementById("guide-button").querySelector("#button")
+    guideButtonPressed = guideButton.getAttribute("aria-pressed")
+    console.log("Hello world!")
+    if (guideButtonPressed) {
+        // Get the home element to remove
+        items = document.getElementById("items").children
+        console.log(items)
+        for (i = 0; i < items.length; i++) {
+            x = items[i].querySelector('[id="endpoint"]')
+            if (x && x.getAttribute("title") == "Home") {
+                items[i].remove() 
+            }
+        }
     }
 }
