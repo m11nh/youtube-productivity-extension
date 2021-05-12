6c01f147b95c3b51ed24bd61c16945daf848c8a5
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-05-12 21:33:27 +1000

    Synchronysed popup with storage (home button)

--- contentScript.js
+++ contentScript.js
@@ -1,2 +1,15 @@
-let items = document.getElementById("items")
-items.remove()
+updateHome()
+function updateHome() {
+    let home_icon_1 = document.querySelector('ytd-mini-guide-entry-renderer[aria-label="Home"]')
+    let home_icon_2 = document.querySelector('a[id="endpoint"][title="Home"]')
+    chrome.storage.sync.get(['home_icon_enabled'], (result) => {
+        let home_icon_enabled = result["home_icon_enabled"]
+        if (home_icon_enabled && home_icon_enabled == true) {
+            home_icon_1.style.display = ""
+            home_icon_2.style.display = ""
+        } else {
+            home_icon_1.style.display = "none"
+            home_icon_2.style.display = "none"
+        }
+    })
+}
--- popup.js
+++ popup.js
@@ -1,17 +1,45 @@
 let home_slider = document.getElementById("home_slider")
-let items = document.getElementById("items")
+// Update home_slider to reflect chrome.storage
+chrome.storage.sync.get(['home_icon_enabled'], async (result) => {
+    if (result.home_icon_enabled == true) {
+        home_slider.checked = false
+    } else {
+        home_slider.checked = true
+    }
+})
 
+
+  
+// Adds event whenever home slider is clicked
 home_slider.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+    // retrieve home button status
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: removeItems,
+        function: handleHomeSliderClick,
     });
 });
 
-function removeItems() {
-    let home1 = document.querySelector('ytd-mini-guide-entry-renderer[aria-label="Home"]')
-    let home2 = document.querySelector('a[id="endpoint"][title="Home"]')
-    if (home1) { home1.remove() }
-    if (home2) { home2.remove() }
+/*
+    If home slider is checked off, update the storage and remove home link, 
+    otherwise if it is enabled, update the storage and un-hide the home link.
+*/
+function handleHomeSliderClick() {
+    let home_icon_1 = document.querySelector('ytd-mini-guide-entry-renderer[aria-label="Home"]')
+    let home_icon_2 = document.querySelector('a[id="endpoint"][title="Home"]')
+    chrome.storage.sync.get(['home_icon_enabled'], (result) => {
+        let home_icon_enabled = result["home_icon_enabled"]
+        if (home_icon_enabled && home_icon_enabled == true) {
+            chrome.storage.sync.set({'home_icon_enabled': false}, () => {
+                home_icon_1.style.display = "none"
+                home_icon_2.style.display = "none"
+            })
+        } else {
+            chrome.storage.sync.set({'home_icon_enabled': true}, () => {
+                if (home_icon_2) { home_icon_2.style.display = "" }
+                if (home_icon_1) { home_icon_1.style.display = "" }
+            })
+        }
+    })
 }
+
