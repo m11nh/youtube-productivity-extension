8746bcdcb368b28fd4d61740393b8fa5e39d55e4
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-05-17 16:59:15 +1000

    added in recommendations turn off on feature

--- contentScript.js
+++ contentScript.js
@@ -1,51 +1,13 @@
-// update the home depending on what's inside storage
-// use mutationObserver
-// const targetNode = document.getElementById('items') 
-// const config = { attributes: true, childList: true, subtree: true }
-// const callback = function(mutationsList, observer) {
-//     console.log("observing")
-//     console.log(mutationsList)
-//     for (const mutation in mutationsList) {
-//         if (mutation.type === 'childList') {
-//             console.log('A child node has been added or removed')
-//         }
-//         else if (mutation.type === 'attributes') {
-//             console.log('The ' + mutation.attributeName + ' attribute was modified.')
-//         }
-//     }
-// }
-// const observer = new MutationObserver(callback)
-// observer.observe(targetNode, config)
-
 updateNavigation()
-// inject content script into guide button, that executes whenever button is clicked
-/*
-    - get the button
-    - add event listener, when clicked, 
-        - determine if guite menu has been toggled
-        - if yes
-            - get home_icon_enabled from storage
-            - update the icon accordingly 
-*/ 
-// let navigation_toggle = document.getElementById("navigation_toggle").querySelector('[id="button"]')
-// guide_button.addEventListener('click', () => {
-//     chrome.storage.sync.get(['navigation_enabled'], (result) => {
-//         let navigation_enabled = result["navigation_enabled"]
-//         let home_icon_2 = document.querySelector('a[id="endpoint"][title="Home"]')
-//         if (navigation_enabled == false) {
-//             home_icon_2.style.display = "none"
-//         }
-//     })
-// })
 
 function updateNavigation() {
     let navigation_section_1 = document.querySelector('ytd-mini-guide-renderer')
     let navigation_section_2 = document.querySelector('div[id="contentContainer"]')
     let navigation_button = document.querySelector('yt-icon-button[id="guide-button"]')
     let recommended_content = document.querySelector('ytd-browse')
-    chrome.storage.sync.get(['navigation_enabled'], (result) => {
-        let navigation_enabled = result["navigation_enabled"]
-        if (navigation_enabled == true) {
+    chrome.storage.sync.get(['navigation'], (result) => {
+        let navigation = result["navigation"]
+        if (navigation == true) {
             navigation_section_1.style.display = ""
             navigation_section_2.style.display = ""
             navigation_button.style.display = ""
--- popup.js
+++ popup.js
@@ -1,45 +1,53 @@
 let navigation_toggle = document.getElementById("navigation_toggle")
-// Update navigation_toggle to reflect chrome.storage
-chrome.storage.sync.get(['navigation_enabled'], async (result) => {
-    if (result.home_icon_enabled == true) {
-        navigation_toggle.checked = false
-    } else {
-        navigation_toggle.checked = true
-    }
-})
+let recommendations_toggle = document.getElementById("recommendations_toggle")
+// let comments_toggle = document.getElementById("comments_toggle")
 
+function updateToggle(toggle_element, toggle_name) {
+    chrome.storage.sync.get([toggle_name], async (result) => {
+        if (result.toggle_name == false) {
+            toggle_element.checked = false
+        } else {
+            toggle_element.checked = true
+        }
+    })
+}
 
-  
-// Adds event whenever home slider is clicked
-navigation_toggle.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    // retrieve home button status
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: handleNavigationSliderClick,
+updateToggle(navigation_toggle, "navigation")
+updateToggle(recommendations_toggle, "recommendations")
+// updateToggle(comments_toggle, "comments")
+
+function addToggleEventListener(toggle_element, event_handler) {
+    toggle_element.addEventListener("click", async () => {
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+        // retrieve home button status
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: event_handler,
+        });
     });
-});
-
+}
+addToggleEventListener(navigation_toggle, handleNavigationToggleClick)
+addToggleEventListener(recommendations_toggle, handleRecommendationsToggleClick)
 /*
     If home slider is checked off, update the storage and remove home link, 
     otherwise if it is enabled, update the storage and un-hide the home link.
 */
-function handleNavigationSliderClick() {
+function handleNavigationToggleClick() {
     let navigation_section_1 = document.querySelector('ytd-mini-guide-renderer')
     let navigation_section_2 = document.querySelector('div[id="contentContainer"]')
     let navigation_button = document.querySelector('yt-icon-button[id="guide-button"]')
     let recommended_content = document.querySelector('ytd-browse')
-    chrome.storage.sync.get(['home_icon_enabled'], (result) => {
-        let home_icon_enabled = result["home_icon_enabled"]
-        if (home_icon_enabled == true) {
-            chrome.storage.sync.set({'home_icon_enabled': false}, () => {
+    chrome.storage.sync.get(['navigation'], (result) => {
+        let navigation = result["navigation"]
+        if (navigation == true) {
+            chrome.storage.sync.set({'navigation': false}, () => {
                 navigation_section_1.style.display = "none"
                 navigation_section_2.style.display = "none"
                 navigation_button.style.display = "none"
                 recommended_content.style.display = "none"
             })
         } else {
-            chrome.storage.sync.set({'home_icon_enabled': true}, () => {
+            chrome.storage.sync.set({'navigation': true}, () => {
                 if (navigation_section_1) { navigation_section_1.style.display = "" }
                 if (navigation_section_2) { navigation_section_2.style.display = "" }
                 if (navigation_button) { navigation_button.style.display = "" }
@@ -48,4 +56,22 @@ function handleNavigationSliderClick() {
         }
     })
 }
+
+function handleRecommendationsToggleClick() {
+    let recommended_videos_element = document.querySelector('div[id="related"]')
+    console.log(recommended_videos_element)
+    chrome.storage.sync.get(['recommendations'], (result) => {
+        let recommendations = result["recommendations"]
+        if (recommendations == true) {
+            chrome.storage.sync.set({'recommendations': false}, () => {
+                recommended_videos_element.style.display = "none"
+            })
+        } else {
+            chrome.storage.sync.set({'recommendations': true}, () => {
+                if (recommended_videos_element) { recommended_videos_element.style.display = "" }
+            })
+        }
+    })
+}
+
 
