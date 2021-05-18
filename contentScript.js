5d734a28689cd08ec5ab24f987b9a905676bfdc4
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-05-18 13:23:17 +1000

    completed proper toggling for navigation and recommendations

--- contentScript.js
+++ contentScript.js
@@ -1,22 +1,31 @@
-updateNavigation()
+updateToggleElements('navigation', getNavigationElements())
+updateToggleElements('recommendations', getRecommendationElements())
 
-function updateNavigation() {
+function getNavigationElements() {
     let navigation_section_1 = document.querySelector('ytd-mini-guide-renderer')
     let navigation_section_2 = document.querySelector('div[id="contentContainer"]')
     let navigation_button = document.querySelector('yt-icon-button[id="guide-button"]')
     let recommended_content = document.querySelector('ytd-browse')
-    chrome.storage.sync.get(['navigation'], (result) => {
-        let navigation = result["navigation"]
-        if (navigation == true) {
-            navigation_section_1.style.display = ""
-            navigation_section_2.style.display = ""
-            navigation_button.style.display = ""
-            recommended_content.style.display = ""
+    return [navigation_section_1, navigation_section_2, navigation_button, recommended_content]
+}
+
+function getRecommendationElements() {
+    let recommended_videos_element = document.querySelector('div[id="related"]')
+    return [recommended_videos_element]
+}
+
+function updateToggleElements(toggle_name, elements) {
+    chrome.storage.sync.get([toggle_name], (result) => {
+        let show_content = result[toggle_name]
+        if (show_content == true) {
+            for (const e of elements) {
+                e.style.display = ""
+            }
+            console.log(show_content, toggle_name)
         } else {
-            navigation_section_1.style.display = "none"
-            navigation_section_2.style.display = "none"
-            navigation_button.style.display = "none"
-            recommended_content.style.display = "none"
+            for (const e of elements) {
+                e.style.display = "none"
+            }
         }
     })
 }
