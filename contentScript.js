302bc959016b9331751c571983f06a3d4eb00090
Duy Minh Nguyen <greatwhitepup@hotmail.com>
2021-05-18 15:04:31 +1000

    completed project

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,8 @@
 updateToggleElements('navigation', getNavigationElements())
-updateToggleElements('recommendations', getRecommendationElements())
+updateToggleElements('recommendations', getRecommendationsElements())
+updateToggleElements('comments', getCommentsElements())
 
+// returns list of navigational elements on the DOM
 function getNavigationElements() {
     let navigation_section_1 = document.querySelector('ytd-mini-guide-renderer')
     let navigation_section_2 = document.querySelector('div[id="contentContainer"]')
@@ -9,11 +11,19 @@ function getNavigationElements() {
     return [navigation_section_1, navigation_section_2, navigation_button, recommended_content]
 }
 
-function getRecommendationElements() {
-    let recommended_videos_element = document.querySelector('div[id="related"]')
-    return [recommended_videos_element]
+// returns list containing recommended videos related elements on the DOM
+function getRecommendationsElements() {
+    let recommended_videos_section = document.querySelector('div[id="related"]')
+    return [recommended_videos_section]
 }
 
+// returns list containing comments related elements on the DOM
+function getCommentsElements() {
+    let comment_section = document.querySelector('ytd-comments[id="comments"]')
+    return [comment_section] 
+}
+
+// given a toggle name and elements, hides or unhides the elements depending on whether the toggle is active or not
 function updateToggleElements(toggle_name, elements) {
     chrome.storage.sync.get([toggle_name], (result) => {
         let show_content = result[toggle_name]
@@ -21,7 +31,6 @@ function updateToggleElements(toggle_name, elements) {
             for (const e of elements) {
                 e.style.display = ""
             }
-            console.log(show_content, toggle_name)
         } else {
             for (const e of elements) {
                 e.style.display = "none"
--- popup.js
+++ popup.js
@@ -1,10 +1,20 @@
 let navigation_toggle = document.getElementById("navigation_toggle")
-let recommendations_toggle = document.getElementById("recommendations_toggle")
-// let comments_toggle = document.getElementById("comments_toggle")
+updateToggle(navigation_toggle, "navigation")
+addToggleEventListener(navigation_toggle, handleNavigationToggleClick)
 
+let recommendations_toggle = document.getElementById("recommendations_toggle")
+updateToggle(recommendations_toggle, "recommendations")
+addToggleEventListener(recommendations_toggle, handleRecommendationsToggleClick)
+
+let comments_toggle = document.getElementById("comments_toggle")
+updateToggle(comments_toggle, "comments")
+addToggleEventListener(comments_toggle, handleCommentsToggleClick)
+
+// Updates the toggle icon in the popup 
 function updateToggle(toggle_element, toggle_name) {
-    chrome.storage.sync.get([toggle_name], async (result) => {
-        if (result.toggle_name == false) {
+    chrome.storage.sync.get([toggle_name], (result) => {
+        let show_content = result[toggle_name]
+        if (show_content == true) {
             toggle_element.checked = false
         } else {
             toggle_element.checked = true
@@ -12,34 +22,26 @@ function updateToggle(toggle_element, toggle_name) {
     })
 }
 
-updateToggle(navigation_toggle, "navigation")
-updateToggle(recommendations_toggle, "recommendations")
-// updateToggle(comments_toggle, "comments")
-
+// adds an event handler to a provided toggle element
 function addToggleEventListener(toggle_element, event_handler) {
     toggle_element.addEventListener("click", async () => {
         let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-        // retrieve home button status
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: event_handler,
         });
     });
 }
-addToggleEventListener(navigation_toggle, handleNavigationToggleClick)
-addToggleEventListener(recommendations_toggle, handleRecommendationsToggleClick)
-/*
-    If home slider is checked off, update the storage and remove home link, 
-    otherwise if it is enabled, update the storage and un-hide the home link.
-*/
+
+// event handler to deal with clicking on navigation toggle
 function handleNavigationToggleClick() {
     let navigation_section_1 = document.querySelector('ytd-mini-guide-renderer')
     let navigation_section_2 = document.querySelector('div[id="contentContainer"]')
     let navigation_button = document.querySelector('yt-icon-button[id="guide-button"]')
     let recommended_content = document.querySelector('ytd-browse')
     chrome.storage.sync.get(['navigation'], (result) => {
-        let navigation = result["navigation"]
-        if (navigation == true) {
+        let show_content = result["navigation"]
+        if (show_content == true) {
             chrome.storage.sync.set({'navigation': false}, () => {
                 navigation_section_1.style.display = "none"
                 navigation_section_2.style.display = "none"
@@ -57,12 +59,12 @@ function handleNavigationToggleClick() {
     })
 }
 
+// event handler to deal with clicking on recommendations toggle
 function handleRecommendationsToggleClick() {
     let recommended_videos_element = document.querySelector('div[id="related"]')
-    console.log(recommended_videos_element)
     chrome.storage.sync.get(['recommendations'], (result) => {
-        let recommendations = result["recommendations"]
-        if (recommendations == true) {
+        let show_content = result["recommendations"]
+        if (show_content == true) {
             chrome.storage.sync.set({'recommendations': false}, () => {
                 recommended_videos_element.style.display = "none"
             })
@@ -74,4 +76,20 @@ function handleRecommendationsToggleClick() {
     })
 }
 
+// event handler to deal with clicking on comments toggle
+function handleCommentsToggleClick() {
+    let comments_element = document.querySelector('ytd-comments[id="comments"]')
+    chrome.storage.sync.get(['comments'], (result) => {
+        let show_content = result["comments"]
+        if (show_content == true) {
+            chrome.storage.sync.set({'comments': false}, () => {
+                comments_element.style.display = "none"
+            })
+        } else {
+            chrome.storage.sync.set({'comments': true}, () => {
+                if (comments_element) { comments_element.style.display = "" }
+            })
+        }
+    })
+}
 
